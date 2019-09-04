e6f258ede9e56fa239f133ac9d5a11da595280c1
Barry Trout <barrytrout@gmail.com>
2019-09-04 21:49:11 +0000

    my second commit

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Cats are a pain!. 
+  Welcome to ${PREFIX}'s app. Cats are really a big pain!. 
   <!-- END -->
   
   </div>
